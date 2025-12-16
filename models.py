c34a733382bdcd4bcd20038c3c6b9dd9dbab5408
Joshua Argel Detchaca <20210021523@my.xu.edu.ph>
2025-12-16 17:59:11 +0800

    update model.py

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ db = client["submission_system"]
 # Security
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
